10e4731fb8e0916e9ea2de281df2d27b02ca07aa
juliahwang <qufskan9396@gmail.com>
2017-05-29 21:29:01 +0900

    TEMPLATE_DIR setting, add post_list view

--- django_app/mysite/settings.py
+++ django_app/mysite/settings.py
@@ -14,7 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# print('BASE_DIR: ', BASE_DIR)
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+# print('TEMPLATES_DIR: ', TEMPLATES_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

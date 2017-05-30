d17fa2ec8b9778363c66f99fb035ab8edfc7a664
juliahwang <qufskan9396@gmail.com>
2017-05-30 15:36:20 +0900

    post_create POST request make post

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,17 +1,19 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.utils import timezone
+from django.contrib.auth import get_user_model
+# from django.http import HttpResponse
+from django.shortcuts import render, redirect
+# from django.utils import timezone
 from .models import Post
 # from 다음의 .은 현재 디렉토리 또는 애플리케이션을 의미
 # 동일한 디렉토리 내의 파일을 불러올 경우 .을 써준다
 
+
+User = get_user_model()
 # Create your views here.
 
 
 def post_list(request):
     # return HttpResponse('<html><body>Post List</body></html>')
-    posts = Post.objects.filter(
-        published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.all().order_by('-created_date')
 
     context = {
         'title': 'Post list from post_list view',
@@ -31,7 +33,19 @@ def post_detail(request, pk):
 
 
 def post_create(request):
-    context = {
+    if request.method == 'GET':
+        context = {
 
-    }
-    return render(request, 'blog/post_create.html', context)
+        }
+        return render(request, 'blog/post_create.html', context)
+    elif request.method == 'POST':
+        data = request.POST
+        title = data['title']
+        text = data['text']
+        user = User.objects.first()
+        post = Post.objects.create(
+            author=user,
+            title=title,
+            text=text
+        )
+        return redirect('post_detail', pk=post.pk)
